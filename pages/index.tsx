2e21723bb7378f70c9d2a77e5e4d60121490efaa
軽部 勝仁 <katsuhito_karube@waku-2.com>
2022-12-17 20:32:14 +0900

    memo types

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,6 +8,12 @@ const baseSchema = z.object({
 
 // merge ✗
 // and ○
+/**
+ * {
+    hobby?: string | undefined;
+    has_hobby: boolean;
+}
+ */
 // const conditionalSchema = z
 //   .object({
 //     has_hobby: z.boolean(),
@@ -28,25 +34,43 @@ const baseSchema = z.object({
 
 // merge ✗
 // and ○
-// const conditionalSchema = z
-//   .object({
-//     has_hobby: z.boolean(),
-//     hobby: z.string().optional(),
-//   })
-//   .transform(({ has_hobby, hobby }, ctx) => {
-//     if (has_hobby && !hobby) {
-//       ctx.addIssue({
-//         path: ["hobby"],
-//         code: z.ZodIssueCode.custom,
-//         message: "入力してください",
-//       });
-//     }
-//     // superRefine との違いは return で変換ができること
-//     return;
-//   });
+/**
+ * {
+    has_hobby: boolean;
+    hobby: string | undefined;
+}
+ */
+const conditionalSchema = z
+  .object({
+    has_hobby: z.boolean(),
+    hobby: z.string().optional(),
+  })
+  .transform(({ has_hobby, hobby }, ctx) => {
+    if (has_hobby && !hobby) {
+      ctx.addIssue({
+        path: ["hobby"],
+        code: z.ZodIssueCode.custom,
+        message: "入力してください",
+      });
+    }
+    // superRefine との違いは return で変換ができること
+    return {
+      has_hobby,
+      hobby,
+    };
+  });
 
 // merge ✗
 // and ○
+/**
+{
+    has_hobby: true;
+    hobby: string;
+} | {
+    hobby?: string | undefined;
+    has_hobby: false;
+}
+ */
 // const conditionalSchema = z
 //   .object({
 //     has_hobby: z.literal(true),
@@ -61,6 +85,15 @@ const baseSchema = z.object({
 
 // merge ✗
 // and ○
+/**
+{
+    has_hobby: true;
+    hobby: string;
+} | {
+    hobby?: string | undefined;
+    has_hobby: false;
+}
+ */
 // const conditionalSchema = z.union([
 //   z.object({
 //     has_hobby: z.literal(true),
@@ -72,18 +105,29 @@ const baseSchema = z.object({
 //   }),
 // ]);
 
+type A = z.infer<typeof conditionalSchema>;
+
 // merge ✗
 // and ○
-const conditionalSchema = z.discriminatedUnion("has_hobby", [
-  z.object({
-    has_hobby: z.literal(true),
-    hobby: z.string().min(1, "入力してください"),
-  }),
-  z.object({
-    has_hobby: z.literal(false),
-    hobby: z.string().optional(),
-  }),
-]);
+/**
+    {
+      has_hobby: true;
+      hobby: string;
+    } | {
+      hobby?: string | undefined;
+      has_hobby: false;
+    }
+ */
+// const conditionalSchema = z.discriminatedUnion("has_hobby", [
+//   z.object({
+//     has_hobby: z.literal(true),
+//     hobby: z.string().min(1, "入力してください"),
+//   }),
+//   z.object({
+//     has_hobby: z.literal(false),
+//     hobby: z.string().optional(),
+//   }),
+// ]);
 
 // merge ○
 // and ○
@@ -91,6 +135,8 @@ const conditionalSchema = z.discriminatedUnion("has_hobby", [
 //   has_hobby: z.boolean(),
 //   hobby: z.string().min(1, "入力してください"),
 // });
+
+type Schema = z.infer<typeof conditionalSchema>;
 const schemaMerge = baseSchema.merge(conditionalSchema);
 const schema = baseSchema.and(conditionalSchema);
 const schemaIntersection = z.intersection(baseSchema, conditionalSchema);
